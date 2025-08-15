3d4628eaae8f34a37082f4e17c9888dff09ec8f7
Pushkin148 <pushkinsharma148@gmail.com>
2025-08-15 14:13:22 +0000

    Added NAT Gateway and Private subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,3 +105,55 @@ resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
+
+
+# Elastic IP for NAT Gateway 
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "${var.cluster_name}-nat-eip"
+  }
+}
+
+# NAT Gateway 
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    Name = "${var.cluster_name}-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+
+# Private Routh Table 
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-private-rt"
+  }
+}
+
+# Associate Private subnets with Private Route Table
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,11 @@ output "internet_gateway_id" {
 output "public_route_table_id" {
   value = aws_route_table.public.id
 }
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.main.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
